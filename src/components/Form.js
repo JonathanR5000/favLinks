3ed886e5ce52ca7281848b92e2aba5f027e53118
JonathanR5000 <jonathan.rivera4@lc.cuny.edu>
2020-10-06 23:25:08 -0400

    added component binds to the constructor class of the form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,30 +1,28 @@
 import React, {Component} from 'react';
 
-class Form extends Component { // TODO - set initial state for link name and URL 
+class Form extends Component {
     constructor(props) {
         super(props);
         this.setState = {
             linkName: '',
             URL: '',
         };
+        this.handleChange = this.handleChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
     }
 
-    handleChange = event => {       // TODO - Logic for changing state based on form changes
-        this.setState({
-            linkName: event.target.value,
-            URL: event.target.value
+    handleChange = event => {
+        this.setState({ linkName: event.target.linkName, URL: event.target.linkName
         });
     };
 
-    onFormSubmit = (event) => {     // TODO - Logic for calling props to handle submission and setting state changes    
+    onFormSubmit = (event) => {   
         event.preventDefault();     // to prevent page reload on form submit
-        
         const submission = 
         {
             linkName : this.state.linkName,
             URL: this.state.URL
         };
-
         this.props.handleChange(submission);
     }
 
@@ -36,14 +34,14 @@ class Form extends Component { // TODO - set initial state for link name and URL
                 <input
                 name="linkName"
                 type="text"
-                value={this.state.linkName}
+                // value={this.state.linkName}
                 onChange={this.handleChange}
                 />
                 <p> URL </p>
                 <input
                 name="URL"
                 type="url"
-                value={this.state.URL}
+                // value={this.state.URL}
                 onChange={this.handleChange}
                 />
 
